2898ad720a8949225df9b54eab4dec08d63906ff
Logan Finley <finley4.logan@gmail.com>
2023-03-02 22:50:39 -0800

    Finish basic motor driver (stepper_rev and stepper_step)

--- src/main.c
+++ src/main.c
@@ -5,4 +5,13 @@
 int main()
 {
     stepper_init();
+
+    while (true) {
+        stepper_step(50, true);
+        sleep_ms(200);
+        stepper_step(50, false);
+        sleep_ms(200);
+        stepper_rev(true);
+        sleep_ms(400);
+    }
 }
--- src/stepper.c
+++ src/stepper.c
@@ -7,6 +7,9 @@ const uint stepper_coil_2_pin = 0;
 const uint stepper_coil_3_pin = 1;
 const uint stepper_coil_4_pin = 7;
 
+const uint stepper_rev_steps = 524; // approximate
+
+// initialize all of the necessary pins
 void stepper_init()
 {
     gpio_init(stepper_coil_1_pin);
@@ -18,4 +21,57 @@ void stepper_init()
     gpio_set_dir(stepper_coil_2_pin, GPIO_OUT);
     gpio_set_dir(stepper_coil_3_pin, GPIO_OUT);
     gpio_set_dir(stepper_coil_4_pin, GPIO_OUT);
+}
+
+// spins the motor 1 step per call in either direction
+void stepper_step(uint steps, bool clockwise)
+{
+    uint delay = 1;
+
+    if (clockwise) {
+        for (int i = 0; i < steps; i++) {
+            gpio_put(stepper_coil_4_pin, 1);
+            sleep_ms(delay);
+            gpio_put(stepper_coil_1_pin, 0);
+            sleep_ms(delay);
+            gpio_put(stepper_coil_3_pin, 1);
+            sleep_ms(delay);
+            gpio_put(stepper_coil_4_pin, 0);
+            sleep_ms(delay);
+            gpio_put(stepper_coil_2_pin, 1);
+            sleep_ms(delay);
+            gpio_put(stepper_coil_3_pin, 0);
+            sleep_ms(delay);
+            gpio_put(stepper_coil_1_pin, 1);
+            sleep_ms(delay);
+            gpio_put(stepper_coil_2_pin, 0);
+            sleep_ms(delay);
+        }
+        return;
+    }
+
+    for (int i = 0; i < steps; i++) {
+        gpio_put(stepper_coil_1_pin, 1);
+        sleep_ms(delay);
+        gpio_put(stepper_coil_4_pin, 0);
+        sleep_ms(delay);
+        gpio_put(stepper_coil_2_pin, 1);
+        sleep_ms(delay);
+        gpio_put(stepper_coil_1_pin, 0);
+        sleep_ms(delay);
+        gpio_put(stepper_coil_3_pin, 1);
+        sleep_ms(delay);
+        gpio_put(stepper_coil_2_pin, 0);
+        sleep_ms(delay);
+        gpio_put(stepper_coil_4_pin, 1);
+        sleep_ms(delay);
+        gpio_put(stepper_coil_3_pin, 0);
+        sleep_ms(delay);
+    }
+}
+
+// spins the motor one full revolution in either direction
+void stepper_rev(bool clockwise)
+{
+    stepper_step(stepper_rev_steps, clockwise);
 }
